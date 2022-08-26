f0927a845eb5c1b5e2ac1706508ebe688d0a9c87
mikef <mike.fullmore@propelplm.com>
2022-08-26 13:00:28 -0700

    more updates to export

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * adding the key objects to exports
  */
 
+const PropelConnect = require('./lib/PropelConnect')
 module.exports = {
     propel_connect: require('./lib/PropelConnect'),
     propel_helper: require('./lib/PropelHelper'),
     propel_log: require('./lib/PropelLog'),
+    new_connection: (hostUrl, sessionId) => {
+      return new PropelConnect(hostUrl, sessionId)
+    }
 }
