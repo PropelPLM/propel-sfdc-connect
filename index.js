67e0c984162b50b8f3b83cef45d2233ae56324a0
mikef <mike.fullmore@propelplm.com>
2022-08-30 17:28:55 -0700

    more changes to logging

--- index.js
+++ index.js
@@ -16,7 +16,4 @@ module.exports = {
   newLog: (errors, id, isInsert, objName, rowName, success) => {
     return new PropelLog(errors, id, isInsert, objName, rowName, success)
   },
-  createEventId: orgId => {
-    return PropelHelper.createEventId(orgId)
-  }
 }
