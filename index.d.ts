e12ec254b84b5a4b411108180b65a02bb8e3079e
mikef <mike.fullmore@propelplm.com>
2022-08-31 14:09:06 -0700

    removing the .d.ts file for now

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-const PropelConnect = import('./dist/PropelConnect.d.ts')
-const PropelHelper = import('./dist/PropelHelper.d.ts')
-const PropelLog = import('./dist/PropelLog.d.ts')
-
-export = {
-    PropelConnect,
-    PropelHelper,
-    PropelLog,
-}
